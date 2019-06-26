66e435e0d5841381fbea74bff66951b848929d6e
kassanj <kassandram022@gmail.com>
2019-06-26 09:34:35 -0700

    style featured mix and audio player sections

--- src/components/App.js
+++ src/components/App.js
@@ -5,13 +5,18 @@ import Header from './Header';
 function App() {
   return (
     <div>
-      <div>
+      <div className="flex-l justify-end">
         <FeaturedMix />
         <div>
           <Header />
         </div>
       </div>
-      <iframe width="100%" height="60" src="https://www.mixcloud.com/widget/iframe/?hide_cover=1&mini=1&feed=%2FNTSRadio%2Fboards-of-canada-societas-x-tape-warp-30-23rd-june-2019%2F" frameBorder="0" ></iframe>
+      <iframe
+        width="100%"
+        height="60"
+        src="https://www.mixcloud.com/widget/iframe/?hide_cover=1&mini=1&feed=%2FNTSRadio%2Fboards-of-canada-societas-x-tape-warp-30-23rd-june-2019%2F"
+        frameBorder="0"
+        className="player db fixed bottom-0"></iframe>
     </div>
   );
 }

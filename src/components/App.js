31c18882014e7561e8b91db5db5cd321dfd3e333
kassanj <kassandram022@gmail.com>
2019-06-26 10:05:14 -0700

    add react router and configure router, navlinks

--- src/components/App.js
+++ src/components/App.js
@@ -1,24 +1,37 @@
-import React from 'react';
+import React, {Component} from 'react';
+import { BrowserRouter as Router, Route } from "react-router-dom";
 import FeaturedMix from './FeaturedMix';
 import Header from './Header';
 
-function App() {
-  return (
-    <div>
-      <div className="flex-l justify-end">
-        <FeaturedMix />
-        <div className="w-50-l relative z-1">
-          <Header />
+
+const Home = () => <h1>Home</h1>
+const Archive = () => <h1>Archive</h1>
+const About = () => <h1>About</h1>
+
+class App extends Component {
+  render() {
+    return (
+      <Router>
+        <div>
+          <div className="flex-l justify-end">
+            <FeaturedMix />
+            <div className="w-50-l relative z-1">
+              <Header />
+              <Route exact path="/" component={Home} />
+              <Route path="/archive" component={Archive} />
+              <Route path="/about" component={About} />
+            </div>
+          </div>
+          <iframe
+            width="100%"
+            height="60"
+            src="https://www.mixcloud.com/widget/iframe/?hide_cover=1&mini=1&feed=%2FNTSRadio%2Fboards-of-canada-societas-x-tape-warp-30-23rd-june-2019%2F"
+            frameBorder="0"
+            className="player db fixed bottom-0"></iframe>
         </div>
-      </div>
-      <iframe
-        width="100%"
-        height="60"
-        src="https://www.mixcloud.com/widget/iframe/?hide_cover=1&mini=1&feed=%2FNTSRadio%2Fboards-of-canada-societas-x-tape-warp-30-23rd-june-2019%2F"
-        frameBorder="0"
-        className="player db fixed bottom-0"></iframe>
-    </div>
-  );
+      </Router>
+    );
+  }
 }
 
 export default App;
--- src/components/Header.js
+++ src/components/Header.js
@@ -1,24 +1,24 @@
 import React, {Component} from 'react';
-
+import { NavLink } from "react-router-dom";
 
 const Header = (props) => (
   <header className="black mb5 pt5">
     <p className="ttu f3 tracked-mega anton tc mt0 mb3">Marmalade.fm</p>
     <ul className="list flex justify-center pl0">
       <li className="mh2">
-         <a href="" className="nav-link link biryani-black f6 ttu gray">
+         <NavLink exact to="/" className="nav-link link biryani-black f6 ttu gray">
             What's hot
-         </a>
+         </NavLink>
       </li>
       <li className="mh2">
-        <a href="" className="nav-link link biryani-black f6 ttu gray">
+        <NavLink to="/archive" className="nav-link link biryani-black f6 ttu gray">
           Archive
-        </a>
+        </NavLink>
       </li>
       <li className="mh2">
-        <a href="" className="nav-link link biryani-black f6 ttu gray">
+        <NavLink to="/about" className="nav-link link biryani-black f6 ttu gray">
           About
-        </a>
+        </NavLink>
       </li>
     </ul>
   </header>
